cb9f7434d989626bc287559b2659cd462281136f
tmkain <47168227+tmkain@users.noreply.github.com>
2025-02-21 22:46:20 +0900

    Update fz_app_jp.py - shift jis

--- fz_app_jp.py
+++ fz_app_jp.py
@@ -235,7 +235,7 @@ if st.button("⏪ 取り消す"):
 # ==============================
 st.header("📥 CSVダウンロード")
 if not df.empty:
-    csv = df.to_csv(index=False, encoding="cp932")  # 🔹 Use CP932 (Shift JIS compatible)
+    csv = df.to_csv(index=False, encoding="shift-jis")  # 🔹 Use CP932 (Shift JIS compatible)
     st.download_button(label="CSVをダウンロード", data=csv, file_name="fz_data.csv", mime="text/csv")
 
 # ==============================
